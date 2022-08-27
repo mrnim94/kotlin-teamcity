ff03341fae68a88c06ef84614eb96b69aaf65b5f
admin <admin@TeamCity>
2022-08-27 09:10:09 +0000

    TeamCity change in 'Nimtechnology / Market Place' project: Versioned settings configuration updated

---
+++ .teamcity/settings.kts
@@ -0,0 +1,58 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2022.04"
+
+project {
+
+    vcsRoot(HomeTestTiki)
+
+    buildType(BuildDocker)
+}
+
+object BuildDocker : BuildType({
+    name = "Build Docker"
+
+    vcs {
+        root(HomeTestTiki)
+    }
+
+    steps {
+        script {
+            name = "Build Docker image"
+            scriptContent = """
+                pwd
+                ls -la
+            """.trimIndent()
+        }
+    }
+})
+
+object HomeTestTiki : GitVcsRoot({
+    name = "home-test-tiki"
+    url = "https://github.com/mrnim94/home-test-tiki.git"
+    branch = "master"
+})
