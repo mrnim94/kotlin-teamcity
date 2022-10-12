ee8eef586a5af51742992f54d0a5ab3f13f4acc1
mr nim <mrnim@nimtechnology.com>
2022-10-12 13:51:20 +0700

    fix conflict teamcity

--- .teamcity/_self/buildTypes/EchoHelloWorld.kt
+++ .teamcity/_self/buildTypes/EchoHelloWorld.kt
@@ -3,8 +3,10 @@ package _self.buildTypes
 import _self.vcsRoots.DockerDemo
 import _self.vcsRoots.HomeTestTiki
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.buildFeatures.dockerSupport
+import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
 import jetbrains.buildServer.configs.kotlin.buildSteps.python
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 object EchoHelloWorld : BuildType ({
     id("HelloWorld")
