4dbd99d31864d3c3fd2286f7c69560dbae835758
mr nim <mrnim@nimtechnology.com>
2022-10-12 15:01:57 +0700

    add newbuild

--- .teamcity/_self/Project.kt
+++ .teamcity/_self/Project.kt
@@ -5,6 +5,7 @@ import jetbrains.buildServer.configs.kotlin.Project
 object HelloWorldProject : Project({
     name = "Hello Project"
     buildType(_self.buildTypes.EchoHelloWorld)
+    buildType(_self.buildTypes.BeforeBuild)
     vcsRoot(_self.vcsRoots.HomeTestTiki)
     vcsRoot(_self.vcsRoots.DockerDemo)
 })
