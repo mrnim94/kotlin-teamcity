d159766cf8aca3ab58cfc8ac4af7c59174b8af97
mr nim <mrnim@nimtechnology.com>
2022-10-12 13:59:14 +0700

    change branchSpec

--- .teamcity/_self/vcsRoots/HomeTestTiki.kt
+++ .teamcity/_self/vcsRoots/HomeTestTiki.kt
@@ -6,6 +6,11 @@ object HomeTestTiki : GitVcsRoot({
     name = "home-test-tiki"
     url = "https://github.com/mrnim94/home-test-tiki"
     branch = "master"
+    branchSpec = """
+            +:refs/heads/*
+            +:refs/tags/*
+        """.trimIndent()
+    useTagsAsBranches = true
 })
 
 object DockerDemo : GitVcsRoot({
